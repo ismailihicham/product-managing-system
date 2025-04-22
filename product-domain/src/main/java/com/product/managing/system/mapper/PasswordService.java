0547a64f75ae69ca77829d0de7ddc8a9882e723a
ISMAILI <hicham.ismaili@europcar.com>
2025-04-22 16:50:32 +0100

    implement a method of hashing password

--- product-domain/src/main/java/com/product/managing/system/mapper/PasswordService.java
+++ product-domain/src/main/java/com/product/managing/system/mapper/PasswordService.java
@@ -1,22 +1,43 @@
 package com.product.managing.system.mapper;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
 @Service
+@Slf4j
 public class PasswordService {
 
-    private final PasswordEncoder passwordEncoder;
-
-    public PasswordService(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
-
     public String hashPassword(String rawPassword) {
-        return passwordEncoder.encode(rawPassword);
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+
+        KeySpec spec = new PBEKeySpec(rawPassword.toCharArray(), salt, 65536, 256);
+        byte[] hash;
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+
+            hash = factory.generateSecret(spec).getEncoded();
+        } catch (Exception e) {
+            log.error("error while hashing password {}", e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+        return Base64.getEncoder().encodeToString(hash);
     }
 
-    public boolean verifyPassword(String rawPassword, String hashedPassword) {
-        return passwordEncoder.matches(rawPassword, hashedPassword);
+    public boolean verifyPassword(String inputPassword, String hashedPassword) throws Exception {
+        byte[] salt = new byte[16];
+        KeySpec spec = new PBEKeySpec(inputPassword.toCharArray(), salt, 65536, 256);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+
+        byte[] hash = factory.generateSecret(spec).getEncoded();
+        String newHash = Base64.getEncoder().encodeToString(hash);
+
+        return newHash.equals(hashedPassword);
     }
 }
